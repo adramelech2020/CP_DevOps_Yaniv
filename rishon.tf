8ea7a2ced22aaa2cd2ed1228c8e4a7453725ecda
adramelech2020 <74201575+adramelech2020@users.noreply.github.com>
2022-04-17 18:56:13 +0300

    Create rishon.tf

---
+++ rishon.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "AKIAUN6LJ3APD2FGD4MJ"
+  secret_key = "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"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "Checkpoint_instance" {
+  ami = ""
+  instance_type = ""
+}
